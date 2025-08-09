c738962b18ae0626f8e616dc2d8cdf6ab35dc4f8
mojavadb <m.j.babaee98@gmail.com>
2025-08-09 20:48:48 +0330

    some changes

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -6,9 +6,25 @@ const kanit = Kanit({
   weight: ["400", "500", "600", "700", "900"],
 });
 
+const numbers = Array.from({ length: 12 }, (_, i) => i + 1);
+const texts = [
+  "اول",
+  "دوم",
+  "سوم",
+  "چهارم",
+  "پنجم",
+  "ششم",
+  "هفتم",
+  "هشتم",
+  "نهم",
+  "دهم",
+  "یازدهم",
+  "دوازدهم",
+];
+
 function Status() {
   return (
-    <header
+    <section
       className="py-18 max-w-3xl mx-auto text-center w-11/12 flex flex-col 
       justify-center items-center gap-4"
     >
@@ -22,7 +38,14 @@ function Status() {
       <h3 className="flex justify-center items-center text-xl md:text-2xl text-stone-900">
         <span>تا کجا پیش رفته اید؟</span>
       </h3>
-    </header>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 w-full">
+        {texts.map((numb: string, index: number) => (
+          <div key={index} className="p-4 rounded-lg border">
+            درس {numb}
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
 
