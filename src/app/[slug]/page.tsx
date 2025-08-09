50fe65f005601865cf2ade297b2b4393e0ef3903
mojavadb <m.j.babaee98@gmail.com>
2025-08-09 23:15:54 +0330

    some changes

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,10 +1,33 @@
+const BASE_URL = "http://localhost:3000";
+
+type Word = {
+  id: number;
+  pronunciation: string;
+  imageUrl: string;
+  wordPr: string;
+  definitionPr: string;
+  word: string;
+  definition: string;
+  synonymsPr: string[];
+  synonyms: string[];
+  examplesPr: string[];
+  examples: string[];
+};
+
 export default async function Lesson({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
   const { slug } = await params;
-  console.log(slug);
+  const response = await fetch(`${BASE_URL}/api/lsn${slug}`);
+  const { words } = await response.json();
 
-  return <div>hi</div>;
+  return (
+    <main>
+      {words.map((word: Word, index: number) => (
+        <div key={index}>{word.word}</div>
+      ))}
+    </main>
+  );
 }
---
+++ src/components/Word.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Word() {
+  return <div></div>;
+}
+
+export default Word;
