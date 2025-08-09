aedca581b289bb462425596557dbd4d255db0c84
mojavadb <m.j.babaee98@gmail.com>
2025-08-09 15:11:38 +0330

    fixing some problems + using swr for fetching data

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -2,13 +2,46 @@
 
 import React from "react";
 import { Kanit } from "next/font/google";
+import useSWR from "swr";
+
+const BASE_URL = "http://localhost:3000";
+
+type Item = {
+  id: number;
+  sentence: string;
+  translate: string;
+};
+
+type Data = {
+  paragraph_one: Item[];
+  paragraph_two: Item[];
+  paragraph_three: Item[];
+  paragraph_four: Item[];
+};
 
 const kanit = Kanit({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "900"],
 });
 
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
 function Introduction() {
+  const { data, error, isLoading } = useSWR<Data>(
+    `${BASE_URL}/api/intro`,
+    fetcher
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  const paragraphs = [
+    data?.paragraph_one,
+    data?.paragraph_two,
+    data?.paragraph_three,
+    data?.paragraph_four,
+  ];
+
   return (
     <section className="py-18 px-4 bg-blue-50">
       <h2
@@ -23,6 +56,21 @@ function Introduction() {
       >
         توجه: با کلیک روی هر جمله میتوانید ترجمه آنرا ببینید!
       </p>
+      <div className="grid">
+        {paragraphs?.map((paragraph, parIndex) => (
+          <div key={parIndex} className="mb-4">
+            {paragraph?.map((item, itemIndex) => (
+              <span
+                key={itemIndex}
+                className="text-black text-md"
+                onClick={() => alert(item.translate)}
+              >
+                {item.sentence}
+              </span>
+            ))}
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
