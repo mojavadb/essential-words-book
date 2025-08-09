48f571327f60015bde388eb101af5c1f344ca91c
mojavadb <m.j.babaee98@gmail.com>
2025-08-09 20:30:13 +0330

    some changes

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -16,11 +16,11 @@ function Status() {
         className={`${kanit.className} direction-ltr flex justify-center 
         items-center text-4xl sm:text-6xl lg:text-[55px] text-stone-900`}
       >
-        <span>504 absolutely essential words</span>
+        <span>Your status:</span>
       </h1>
 
       <h3 className="flex justify-center items-center text-xl md:text-2xl text-stone-900">
-        <span>۵۰۴ واژه ضروری انگلیسی</span>
+        <span>تا کجا پیش رفته اید؟</span>
       </h3>
     </header>
   );
