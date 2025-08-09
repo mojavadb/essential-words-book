03a87a6270aa7241db68e9071d2248c7c9f06463
mojavadb <m.j.babaee98@gmail.com>
2025-08-09 22:33:37 +0330

    some changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import Logo from "./Logo";
 
 function Footer() {
   return (
-    <footer className="px-8 py-4 sm:px-16 bg-stone-900 text-white flex items-center justify-between">
+    <footer className="px-8 py-4 sm:px-16 bg-stone-900 text-white flex items-start sm:items-center gap-10 justify-between">
       <div>
         <Logo className="w-auto h-auto" />
       </div>
