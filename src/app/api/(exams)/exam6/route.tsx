fccb3f6faca2c86c4a2376da6999a2a5da8df5aa
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 17:03:55 +0330

    fill exam 6

--- src/app/api/(exams)/exam6/route.tsx
+++ src/app/api/(exams)/exam6/route.tsx
@@ -3,63 +3,69 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question:
+          "Each of our workers is trained to give your car a examination. (Which two words might fit this sentence?)",
+        answer: " thorough",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question:
+          "Tom jones was the best singer in the choir when he was young.",
+        answer: "undoubtedly",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question: "He could the problem from all angles.",
+        answer: "approach",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question: "Mrs. Spector always wanted to be with her friends.",
+        answer: "popular",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question: "Why did you cleaning your room today?",
+        answer: "neglect",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question: "The bought his boss a birthday present.",
+        answer: "employee",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question: "Rocco's only was that he walked with a slight limp.",
+        answer: "defect",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question:
+          "None of the other poker players suspected that their friend would them in order to win.",
+        answer: "defraud",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question:
+          "When Cynthia realized that nobody liked her, she knew she had been ___ _",
+        answer: "deceived",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question:
+          "10. I could from the tone of his voice that he was in a bad mood.",
+        answer: "detect",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question: "His was happy with the work Terence had been doing for him.",
+        answer: "client",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "I do not want to do anything less than a job on my term paper. (Which two words might fit this sentence?)",
+        answer: "comprehensive",
       },
     ],
   };
