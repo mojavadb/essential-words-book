76fd867da7b3173bd4d99e9559d65dfd7e06050d
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 18:08:20 +0330

    fill exam 14

--- src/app/api/(exams)/exam14/route.tsx
+++ src/app/api/(exams)/exam14/route.tsx
@@ -3,63 +3,71 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question:
+          "Rory was thrown out of school because of his ______ behavior.",
+        answer: " unruly",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question: "The ______ lion attacked the lost child in the forest.",
+        answer: "brutal",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question: "They had a ______ over who was a better swimmer.",
+        answer: "brawl",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question:
+          "The magician ______ his hand into his hat, and out came a rabbit.",
+        answer: "thrust",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question:
+          "A man was caught trying to ______ documents* that were top secret.",
+        answer: "duplicate",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question:
+          "His ______ was a man who was trying to win the heart of his girl.",
+        answer: "opponent",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question:
+          "The experienced chess player tried to keep his ______ guessing.",
+        answer: "rival",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question:
+          "The boy was ______ by the fact that his parents had abandoned him.",
+        answer: "bewildered",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question: "Whenever the skinny boy got into a fight he was the ______",
+        answer: "underdog",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question:
+          "When some animals aren't fed on time they become very ______",
+        answer: "violent",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question:
+          "The ball was hit so hard that it went ______ down the field.",
+        answer: "whirling",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question: "Five hundred men were killed in that ______ battle.",
+        answer: "vicious",
       },
     ],
   };
