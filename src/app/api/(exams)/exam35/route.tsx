1c2625ed060c85f22378c89783e1ef5f56216869
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 23:30:28 +0330

    fill exam 35

--- src/app/api/(exams)/exam35/route.tsx
+++ src/app/api/(exams)/exam35/route.tsx
@@ -3,63 +3,71 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question:
+          "Our club consented to hold a ______ as a means of raising money.",
+        answer: "lottery",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question:
+          "If you want to ______ data on employment opportunities, a good source is the Bulletin of the Department of Labor.",
+        answer: "obtain",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question: "Some call it a ______ but I call it a movie.",
+        answer: "cinema",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question: "Our team excelled in the last ______ of the track meet.",
+        answer: "event",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question:
+          "As I watched the huge jet ______ into the sky, I wished that I were on board.",
+        answer: "soaring",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question:
+          "Since Margaret has become his neighbor, Bud's progress in school has been practically ______",
+        answer: "stationary",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question:
+          " ______ to his phone call, I received a confirmation in the mail.",
+        answer: "Subsequent",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question: "We are compelled to ______ this outdated theory.",
+        answer: "discard",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question: "How does that evidence ______ to the case?",
+        answer: "relate",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question:
+          "Tim's argument may be logical but it is too ______ to be convincing.",
+        answer: "vague",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question:
+          "With no one to ______ the project, the entire scheme collapsed.",
+        answer: "finance",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "This feeble speech will do little to ______ the spirits of the audience.",
+        answer: "elevate",
       },
     ],
   };
