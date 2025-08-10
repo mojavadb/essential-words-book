16cabc8456be42bb66a6e41d93faa36b90a86c4a
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 17:18:02 +0330

    fill exam 8

--- src/app/api/(exams)/exam8/route.tsx
+++ src/app/api/(exams)/exam8/route.tsx
@@ -3,63 +3,68 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question: "Ernesto would constantly ______ his father's questions.",
+        answer: "weird",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question:
+          "Historical ______ are kept in a special section of the library.",
+        answer: "valid",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question: "Great scientific progress has been made in this ______",
+        answer: "obvious",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question: "The massive wrestler accepted the ______ of the newcomer.",
+        answer: "mediocre",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question: "Not wearing warm clothing was the ______ of his illness.",
+        answer: "reluctant",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question:
+          "When do you expect to ______ your investigation of the case?",
+        answer: "exaggerate",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question: "It is ______ that this restaurant's food is delicious.",
+        answer: "security",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question: "Lena showed ______ a of good judgment.",
+        answer: "bulky",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question:
+          "Everyone who lived in the ______ of the bomb test was in peril.",
+        answer: "amateur",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question:
+          "Anita's habit of interrupting him sent her husband into a ______",
+        answer: "variety",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question: "My nephew was given a set of ______ soldiers for Christmas.",
+        answer: "prominent",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "When you are tired it is hard to ______ staying in bed all day.",
+        answer: "survive",
       },
     ],
   };
