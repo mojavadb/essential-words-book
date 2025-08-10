a3c89fb9fa02f1a918abd8ad6f7af5b55fc60e03
mojavadb <m.j.babaee98@gmail.com>
2025-08-11 02:10:55 +0330

    fill exam 42

--- src/app/api/(exams)/exam42/route.tsx
+++ src/app/api/(exams)/exam42/route.tsx
@@ -3,63 +3,74 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question:
+          "When the third person she approached turned away from her, Marilyn had a vague* feeling she was being ______",
+        answer: "snubbed",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question:
+          "______ Achilles' did not cease* until he had taken revenge on the slayer of his friend.",
+        answer: "wrath",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question:
+          "As the captain ______ their situation, he realized that resistance* was in vain.*",
+        answer: "pondered",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question:
+          "Father ______ that all forms of gambling should be declared illegal.*",
+        answer: "maintained",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question:
+          "The prophet warned that the land cannot ______ such violations of morality.*",
+        answer: "endure",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question:
+          "To diminish* the chances of raising weaklings, the ancient Spartans used to ______ to the elements the babies that they did not want.",
+        answer: "expose",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question:
+          "The Robin Hood stories are a good illustration* of a ______ from the Middle Ages.",
+        answer: "legend",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question:
+          "The ______ is the symbol* of the democratic way of choosing leaders.",
+        answer: "ballot",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question:
+          "Because of the secret nature of the cargo, visits to the ______ area were prohibited.*",
+        answer: "wharf",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question: "The patriot* was ______ dying for his deed.",
+        answer: "resigned",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question:
+          "We are trying to persuade* the sponsors of the bill to ______ it in order to improve its chances for passage.",
+        answer: "amend",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "The guinea pigs could not put up with ______ changes in their diet.",
+        answer: "drastic",
       },
     ],
   };
