09e12101f78c5199480fb6d59044ef5af576cf1c
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 20:17:06 +0330

    fill exam 15

--- src/app/api/(exams)/exam15/route.tsx
+++ src/app/api/(exams)/exam15/route.tsx
@@ -3,63 +3,71 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question: "Dominick was ______ to have such good friends.",
+        answer: "fortunate",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question:
+          "Rhonda didn't believe in divorce because she felt that marriage is ______",
+        answer: "sacred",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question:
+          "The pilot had to ______ his course when he ran into bad weather.",
+        answer: "alter",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question: "Everyone approved of Dave's ______ proposal.",
+        answer: "innovative",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question: "David wanted to ______ medicine as a career.",
+        answer: "pursue",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question:
+          "He moved out of the house when he became a ______ young man.",
+        answer: "mature",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question:
+          "The vote to make Jim president of the camera club was ______",
+        answer: "unanimous",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question:
+          "When his mother died of cancer, the young doctor decided to ______ his life to finding a cure for it.",
+        answer: "pledge",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question:
+          "They had to ______ their plans when a third person decided to join them for lunch.",
+        answer: "revise",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question:
+          "My grandfather was a ______ in the field of sports medicine.",
+        answer: "pioneers",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question:
+          "The relaxed friends spoke in a ______ manner as they talked on the street.",
+        answer: "casual",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question: "I can feel my stomach ______ when I breathe deeply.",
+        answer: "expand",
       },
     ],
   };
