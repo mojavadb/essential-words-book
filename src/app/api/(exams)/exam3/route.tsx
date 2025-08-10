ee641549b03b50eca619c2391ce70cd1249c18d5
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 16:30:16 +0330

    fill exam 3

--- src/app/api/(exams)/exam3/route.tsx
+++ src/app/api/(exams)/exam3/route.tsx
@@ -3,63 +3,71 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question:
+          "The March of Dimes makes its ______ appeal in the early spring.",
+        answer: "annual",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question:
+          "Oil paints ______ easily to form thousands of different shades.",
+        answer: "blend",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question: "The ______ passing mark in most schools is 65%.",
+        answer: "minimum",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question: "The producer always had her eye out for young ______",
+        answer: "talent ",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question:
+          "Your gifts do not tempt me and will not ______ me to change my mind.",
+        answer: "persuade",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question:
+          "In the cemetery the corpse was ______ in the bright moonlight.",
+        answer: "visible",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question:
+          "A ______ day in Florida is full of sunshine and warm breezes.",
+        answer: " typical",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question: "Let's ______ a plan for doing away with homework.",
+        answer: "devise",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question: "Everyone agrees that friendship is ______ for all of us.",
+        answer: "essential ",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question:
+          "A sharp rise in prices is bound to affect the prices in ______ our neighborhood stores.",
+        answer: " wholesale",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question:
+          "The buffalo, which once roamed the plains, is quite ______ today.",
+        answer: "scarce",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "Government experts told us to buy chicken without realizing how ______ it had become.",
+        answer: "expensive",
       },
     ],
   };
