57a07923c301cd95f8aca791237498d3ecfca66d
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 23:36:26 +0330

    fill exam 36

--- src/app/api/(exams)/exam36/route.tsx
+++ src/app/api/(exams)/exam36/route.tsx
@@ -3,63 +3,69 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question: " I insist* on a ______ answer to my question.",
+        answer: "prompt",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question: "Harriet's anger was subsequently ______ by the apology.*",
+        answer: "soothed",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question:
+          "The minister said those who are not ______ from sin will perish.*",
+        answer: "redeemed",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question:
+          "Joseph could not ______ from embracing* his long-lost brother.",
+        answer: "refrain",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question:
+          "My cat and dog, though traditionally* enemies, have lived in perfect ______ for years.",
+        answer: "harmony",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question: "Because he liked to be prompt,* Sal ate only a ______ meal.",
+        answer: "hasty",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question:
+          "The farmers were grateful* that the ______ had not destroyed their harvest.*",
+        answer: "tempest",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question:
+          "Jenny picked up the hot iron just as it was about to ______ my shirt.",
+        answer: "scorch",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question: "The couple seemed so ______ that the breakup baffled* us.",
+        answer: "sympathetic",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question: "Chris Pollaro ______ his former position with the company.",
+        answer: "resumed",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question: "It is ______ to own firearms without a license.",
+        answer: "illegal",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "It is impossible* to estimate* the harm caused by the illegal* use of ______ .",
+        answer: "narcotics",
       },
     ],
   };
