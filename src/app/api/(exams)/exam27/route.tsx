c450d388bf781ff308fa83a54469db11ca7c09ab
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 22:05:22 +0330

    fill exam 27

--- src/app/api/(exams)/exam27/route.tsx
+++ src/app/api/(exams)/exam27/route.tsx
@@ -3,63 +3,73 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question: "The dying soldier made a last ______ effort to rise.",
+        answer: "feeble",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question:
+          "Though the victim's breathing had ______ , the fireman continued giving oxygen.",
+        answer: "ceased",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question:
+          "We often regret a ______ statement made in the heat of an argument.",
+        answer: "rash",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question:
+          "Now that you have ______ all your excuses, tell us the truth.",
+        answer: "exhausted",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question:
+          "The bout was fought to determine who would be the ______ of the ring.",
+        answer: "monarch",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question:
+          "An increase in pollution will ______ our normal ways of life.",
+        answer: "disrupt",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question: "Macy's Department Store always says, It pays to be ______",
+        answer: "thrifty",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question:
+          "I have learned to read Spanish ______ , but I cannot speak it so well.",
+        answer: "rapidly",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question:
+          "The ______ of the pain compelled Frank to call the doctor in the middle of the night.",
+        answer: "severity",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question:
+          "Under certain conditions, oxygen will ______ with hydrogen to form water.",
+        answer: "unite",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question:
+          "______ a warning glance from the teacher, Harold continued to annoy the girl next to him.",
+        answer: "Despite",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "It is best to be neither too ______ nor too careless about one's money.",
+        answer: "miserly",
       },
     ],
   };
