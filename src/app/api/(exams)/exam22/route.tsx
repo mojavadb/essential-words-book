9beb252a4ba8769c76bb18b63da3c6d35771ec6a
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 21:10:58 +0330

    fill exam 22

--- src/app/api/(exams)/exam22/route.tsx
+++ src/app/api/(exams)/exam22/route.tsx
@@ -3,63 +3,69 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question:
+          "Wayne's parents yelled at him because he was known as a ______",
+        answer: "delinquent",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question: "The courts will ______ you if you don't obey the law.",
+        answer: "penalize",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question:
+          "Are you satisfied with your ______ or are you thinking of getting a different job?",
+        answer: "vocation",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question:
+          "Five persons were being questioned by the police about the brutal ______",
+        answer: "homicide",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question: "I ______ the fact that I received the tapes.",
+        answer: "acknowledge",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question:
+          "When Steve asked his girlfriend to marry him, he did not anticipate that she would ______ him.",
+        answer: "reject",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question: "The ______ of the prison set up stricter rules.",
+        answer: "warden",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question: "Mrs. Fried's ______ came home weary after each day's work.",
+        answer: "spouse",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question: "It is often ______ persons who commit serious crimes.",
+        answer: "unstable",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question:
+          "Clara felt as if she had been ______ of the better things in life.",
+        answer: "deprived",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question: "______ was served when the villain was put behind bars.",
+        answer: "Justice",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "joseph was the ______ of large sums of money from his uncle's insurance policy. ",
+        answer: "beneficiary",
       },
     ],
   };
