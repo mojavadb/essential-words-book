1f3e056ad1266609852553f043a93edee2b37fd3
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 22:45:40 +0330

    fill exam 31

--- src/app/api/(exams)/exam31/route.tsx
+++ src/app/api/(exams)/exam31/route.tsx
@@ -3,63 +3,73 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question: "Celia was left ______ after working so hard all her life.",
+        answer: "idle",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question:
+          "I would ______ that claim if I did not know you were jesting.",
+        answer: "dispute",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question:
+          "This site will attract many home buyers because of the advantages for the ______",
+        answer: "commuter",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question:
+          "The early pioneers exhibited great ______ in braving the hardships of the new frontiers.",
+        answer: "valor",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question:
+          "A ______ of caution helped Mr. Samler to avoid the obvious risks in his new job.",
+        answer: "vein",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question:
+          "It is no misfortune to spend a few ______ days without excitement and conflict.",
+        answer: "uneventful",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question:
+          "The ______ element of society ignores the warnings about the dangers of drugs.",
+        answer: "lunatic",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question:
+          "Since the statement was made in ______ it is not a valid point to argue.",
+        answer: "jest",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question:
+          "I asked my opponent in the debate to ______ his remarks to the subject under discussion.",
+        answer: "confine",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question:
+          "How can we transmit a healthy ______ view to the next generation?",
+        answer: "patriotic",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question:
+          "People have always wanted to inhabit the land where the soil is most ______",
+        answer: "fertile",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question: "People still worship the ______ of greed and power.",
+        answer: "idol",
       },
     ],
   };
