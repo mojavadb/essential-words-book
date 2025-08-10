965caccab3c135e6ba9cb88cd5da07580c850ed5
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 16:56:09 +0330

    fill exam 5

--- src/app/api/(exams)/exam5/route.tsx
+++ src/app/api/(exams)/exam5/route.tsx
@@ -3,63 +3,66 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question: " I left the city for a peaceful ______ farm. ",
+        answer: "rural",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question:
+          "Professor Dixon liked the atmosphere of the university ______",
+        answer: "campus",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question: " He tried to ______ questions he didn't know how to answer.",
+        answer: " evade",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question: "The ______ of people wanted him to be president.",
+        answer: "majority",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question: "The guests began to ______ for Thanksgiving dinner.",
+        answer: "assemble",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question: "Christmas trees are a popular ______ for many people.",
+        answer: "tradition",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question: "Making a living for his family was too much of a ______",
+        answer: "burden",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question: "I want to ______ all the cities I haven't visited.",
+        answer: "explore",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question: "If Gene doesn't ______ , he will get into serious trouble.",
+        answer: "reform",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question:
+          "He had to do research on the ______ of biology for a school report.",
+        answer: "topic ",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question: "Historians will ______ the causes of the war in Iraq.",
+        answer: "probe",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "Whether or not eighteen-year-olds should be allowed to vote was in ______ for a long time.",
+        answer: "debate",
       },
     ],
   };
