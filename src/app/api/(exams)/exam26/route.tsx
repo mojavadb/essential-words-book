9ef33fc046ed700ec5bab9927e1f857d3809b224
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 21:41:29 +0330

    fill exam 26

--- src/app/api/(exams)/exam26/route.tsx
+++ src/app/api/(exams)/exam26/route.tsx
@@ -3,63 +3,72 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question:
+          "History has shown us that tyranny cannot ______ the human desire for freedom.",
+        answer: "quench",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question:
+          "The sailors used the North Star to ______ them to their destination.",
+        answer: "guide",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question: "Our eyes ______ to sudden light by blinking.",
+        answer: "respond",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question: "The coach ______ to the pitcher to watch for a bunt.",
+        answer: "beckoned",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question:
+          "Little Benjy was foolish enough to ______ out on the thin ice.",
+        answer: "venture",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question:
+          "If we are ______ to vote without hearing all sides of the issue, we could make a serious mistake.",
+        answer: "compelled",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question:
+          "Power in the hands of the ignorant is an ______ responsibility.",
+        answer: "awesome",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question: "Benedict Arnold ______ his country.",
+        answer: "betrayed",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question:
+          "The only ______ excuse for Barry's failure is his lack of effort.",
+        answer: "possible",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question:
+          "It was not possible to ______ the excited woman after she was fined for jaywalking.",
+        answer: "pacify",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question:
+          "The last words ______ by the dying soldier were, 'We must hold the fort.'",
+        answer: "uttered",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "Ralph found himself in an ______ situation when his blind date turned out to be a foot taller than he.",
+        answer: "awkward",
       },
     ],
   };
