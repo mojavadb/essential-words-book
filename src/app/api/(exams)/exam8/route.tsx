d8d32c73546ee72f9b5233fc075f155feec51c79
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 17:25:22 +0330

    fill exam 8

--- src/app/api/(exams)/exam8/route.tsx
+++ src/app/api/(exams)/exam8/route.tsx
@@ -3,67 +3,65 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "Ernesto would constantly ______ his father's questions.",
+        question:
+          "Most people agreed that he was a looking man because of the long red beard.",
         answer: "weird",
       },
       {
         id: 2,
         question:
-          "Historical ______ are kept in a special section of the library.",
+          "Chuck's reason for quitting his job was ; he was not being paid.",
         answer: "valid",
       },
       {
         id: 3,
-        question: "Great scientific progress has been made in this ______",
+        question: "The answer to the question was so that everyone knew it.",
         answer: "obvious",
       },
       {
         id: 4,
-        question: "The massive wrestler accepted the ______ of the newcomer.",
+        question: "The tennis player would never make the Olympic squad.",
         answer: "mediocre",
       },
       {
         id: 5,
-        question: "Not wearing warm clothing was the ______ of his illness.",
+        question: "She was to take on any more responsibilities at work.",
         answer: "reluctant",
       },
       {
         id: 6,
-        question:
-          "When do you expect to ______ your investigation of the case?",
+        question: "People often tend to stories they hear.",
         answer: "exaggerate",
       },
       {
         id: 7,
-        question: "It is ______ that this restaurant's food is delicious.",
+        question: "The bank is kept under very tight ___ _",
         answer: "security",
       },
       {
         id: 8,
-        question: "Lena showed ______ a of good judgment.",
+        question: "Because the box was so it took two men to lift it.",
         answer: "bulky",
       },
       {
         id: 9,
         question:
-          "Everyone who lived in the ______ of the bomb test was in peril.",
+          "Even though he was not a professional, the photographer entered the contest.",
         answer: "amateur",
       },
       {
         id: 10,
-        question:
-          "Anita's habit of interrupting him sent her husband into a ______",
+        question: "A wide of shows is playing at the concert hall.",
         answer: "variety",
       },
       {
         id: 11,
-        question: "My nephew was given a set of ______ soldiers for Christmas.",
+        question: "Mrs. Meyers is a member of the staff.",
         answer: "prominent",
       },
       {
         id: 12,
-        question:
-          "When you are tired it is hard to ______ staying in bed all day.",
+        question: "We all hoped that the small boat would the storm. ",
         answer: "survive",
       },
     ],
