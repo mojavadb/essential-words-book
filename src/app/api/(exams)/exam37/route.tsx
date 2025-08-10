eaeebc40b8dd2bb5631dfe0a76bd13337b21b708
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 23:44:56 +0330

    fill exam 37

--- src/app/api/(exams)/exam37/route.tsx
+++ src/app/api/(exams)/exam37/route.tsx
@@ -3,63 +3,69 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question: "The exhausted regiment ______ down to a few troops.",
+        answer: "dwindled",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question: "Secret documents listed the ______ to the large fortune.",
+        answer: "heir",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question: "Iris made a ______ attempt to ignore their biting comments.",
+        answer: "deliberate",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question: "The ______ of waste materials has polluted our rivers.",
+        answer: "surplus",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question:
+          "Charles blundered off in the wrong direction without ______ my warning cries.",
+        answer: "heeding",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question: "Lincoln tried in vain to keep the North and South ______",
+        answer: "unified",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question:
+          "It did not take long before the unruly crowd turned into a mob of howling ______",
+        answer: "vandals",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question:
+          "The confirmed bachelor could not ______ having anyone touch a single utensil in his home.",
+        answer: "abide",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question: "If the ______ does not end soon, I can predict a famine.",
+        answer: "drought",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question:
+          "The population rise will reach its ______ in a few years and then it will level off.",
+        answer: "summit",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question:
+          "Lt. Jenkins lost every morsel of self-respect and became a ______ to his flag.",
+        answer: "traitor",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "The loyal captain, ______ in defeat, won the sympathy of the people.",
+        answer: "majestic",
       },
     ],
   };
