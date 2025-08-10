85c6104ec79aad245e6ed0eb1c04f992a9f1423e
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 22:37:37 +0330

    fill exam 30

--- src/app/api/(exams)/exam30/route.tsx
+++ src/app/api/(exams)/exam30/route.tsx
@@ -3,63 +3,75 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question:
+          "The judge ______ to this case must be a person of justice and honesty.",
+        answer: "appointed",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question:
+          "I knew that the culprit was in ______ terror of being caught.",
+        answer: "mortal",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question:
+          "The ______ of the collapsed buil9ing were dazed by the tragedy.",
+        answer: "occupants",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question:
+          "How can you ______ in a favorable light a person who betrayed his country?",
+        answer: "depict",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question:
+          "Let us explore another ______ for the hospital where the population is not so dense.",
+        answer: "site",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question:
+          "A good line of ______ is thrifty with words and bursting with feeling.",
+        answer: "verse",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question:
+          "Troops were ______ in the city in a vain attempt to keep order.",
+        answer: "quartered",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question:
+          "The price he ______ for the car was essentially the same as that of his competitor.",
+        answer: "quoted",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question:
+          "Seeing the movie based on the ______ does not exempt you from reading the book.",
+        answer: "novel",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question:
+          "The death of the millionaire ______ a bewildering number of hopeful beneficiaries.",
+        answer: "attracted",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question:
+          "The new ______ has not won unanimous acceptance; there are those who prefer the traditional ways.",
+        answer: "morality",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "When I ______ abroad, I come across many historic sites and structures. ",
+        answer: "roam",
       },
     ],
   };
