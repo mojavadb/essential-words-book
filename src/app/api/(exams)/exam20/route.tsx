bda2710ad78040d0cbaabb6cdd0fb00eab41e4e8
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 20:50:38 +0330

    fill exam 20

--- src/app/api/(exams)/exam20/route.tsx
+++ src/app/api/(exams)/exam20/route.tsx
@@ -3,63 +3,69 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question:
+          "If we do not do something about pollution, we may ______ from this earth.",
+        answer: "perish",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question: "The ______ edge of that sheet of metal is very dangerous.",
+        answer: "jagged",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question: "We were held ______ by the sinister enemy for ten days.",
+        answer: "captive",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question:
+          "The bank teller's ______ plan to steal a million dollars didn't succeed.",
+        answer: "crafty",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question:
+          "I like to ______ on until everyone else has left the theater.",
+        answer: "linger",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question: "My parents taught me not to be ______ of authority.",
+        answer: "defiant",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question: "Did the ______ of the Lebanese soldiers fail?",
+        answer: "ambush",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question:
+          "Business persons can ______ if they are honest with their customers.",
+        answer: "prosper",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question: "A new star has just ______ from the rock music world.",
+        answer: "emerged",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question: "I can ______ a steak in two minutes when I am hungry.",
+        answer: "devour",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question:
+          "With a surprising show of ______ , the old woman swam up and down the pool six times!",
+        answer: "vigor",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "A lack of calcium in Tyrone's diet caused his bones to be quite ______ ",
+        answer: "fragile",
       },
     ],
   };
