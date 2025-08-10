42cafa008cec59c8f333b86d921392c85dc7a424
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 20:41:48 +0330

    fill exam 19

--- src/app/api/(exams)/exam19/route.tsx
+++ src/app/api/(exams)/exam19/route.tsx
@@ -3,63 +3,66 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question:
+          "The committee recommended that we ______ all dangerous foods.",
+        answer: "ban",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question: "Dave had his ______ of cookies for the day.",
+        answer: "quota",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question:
+          "You should always make sure that you have an ______ supply of gasoline for a long trip.",
+        answer: "abundant",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question: "The rain was ______ into the concrete when it was dry.",
+        answer: "absorbed",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question: "Is this inexpensive dress ______ for a formal wedding?",
+        answer: "appropriate",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question: "How much do you ______ that horse is worth?",
+        answer: "estimate",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question:
+          "Helen had an ______ feeling as she went on to the stage for the first time.",
+        answer: "uneasy",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question: "When you are in trouble the worst thing to do is to ______",
+        answer: "panic",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question: "The farmers had a good ______ of corn this year.",
+        answer: "harvest",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question: "We ______ all the figures and came to one solid answer.",
+        answer: "calculated",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question: "Every ______ the cook prepared was tasty.",
+        answer: "morsel",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question: "The ______ of snow caused us to change our holiday plans.",
+        answer: "threat",
       },
     ],
   };
