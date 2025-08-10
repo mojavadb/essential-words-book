8d34631d6f632f323a3c41efd0718d73fbeafb0f
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 14:59:04 +0330

    some changes

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { MoveLeft, MoveRight, Volume2 } from "lucide-react";
+import { Languages, MoveLeft, MoveRight, Volume2 } from "lucide-react";
 import { useParams } from "next/navigation";
 import useSWR from "swr";
 import { Kanit, Pacifico } from "next/font/google";
@@ -49,7 +49,10 @@ export default function Lesson() {
       {data?.words.map(
         (word, index) =>
           index === indexOfWords && (
-            <div className="p-4" key={index}>
+            <div
+              className="p-4 grid gap-6 md:grid-cols-2 md:gap-16 md:p-12"
+              key={index}
+            >
               <div className="p-3 bg-white mb-3">
                 <div
                   className={`w-60 h-56 mx-auto border border-gray-300 rounded-sm mb-8
@@ -72,7 +75,14 @@ export default function Lesson() {
                   <p className={`text-sm text-gray-500 ${kanit.className}`}>
                     {word.pronunciation}
                   </p>
-                  <Volume2 size={20} className="absolute top-0 right-4" />
+                  <Volume2 size={20} className="absolute top-0 right-12" />
+                  <a
+                    href={`https://translate.google.com/?hl=fa&sl=en&tl=fa&text=${word.word}&op=translate`}
+                    target="_blank"
+                    className="absolute top-0 right-2 text-sm flex items-center gap-2"
+                  >
+                    <Languages size={20} />
+                  </a>
                 </div>
                 <div className="mb-8">
                   <p className="text-xs text-gray-500 mb-2">فارسی</p>
@@ -83,21 +93,26 @@ export default function Lesson() {
                 </div>
                 <p className="text-base">{word.definitionPr}</p>
               </div>
-              {[0, 1, 2].map((i) => (
-                <div key={i} className="p-5 bg-blue-100 mb-4">
-                  <p className={`${kanit.className} direction-ltr mb-4`}>
-                    {i + 1}- {word.examples[i]}
-                  </p>
-                  <p>{word.examplesPr[i]}</p>
-                </div>
-              ))}
+              <div>
+                {[0, 1, 2].map((i) => (
+                  <div key={i} className="p-5 bg-blue-100 mb-4">
+                    <p className={`${kanit.className} direction-ltr mb-4`}>
+                      {i + 1}- {word.examples[i]}
+                    </p>
+                    <p>{word.examplesPr[i]}</p>
+                  </div>
+                ))}
+              </div>
             </div>
           )
       )}
-      <div className="flex items-center justify-between gap-12 px-8 mb-6">
+      <div
+        className="flex items-center justify-between gap-12 px-8 mb-6 md:px-12
+      md:mb-9 md:gap-0 md:justify-around"
+      >
         <button
           type="button"
-          className="flex items-center gap-2 bg-red-800 hover:bg-red-900 text-white 
+          className="flex items-center gap-2 bg-red-800 hover:bg-red-900 text-white cursor-pointer
           font-bold py-2 px-4 rounded-lg text-sm transition-all duration-300 transform 
           hover:scale-105 focus:outline-none focus:ring-2 focus:ring-red-700 focus:ring-opacity-50"
           onClick={() =>
@@ -111,7 +126,7 @@ export default function Lesson() {
         <div className="font-bold text-lg">{indexOfWords + 1}</div>
         <button
           type="button"
-          className="flex items-center gap-2 bg-green-800 hover:bg-green-900 text-white 
+          className="flex items-center gap-2 bg-green-800 hover:bg-green-900 text-white cursor-pointer 
           font-bold py-2 px-4 rounded-xl text-sm transition-all duration-300 transform 
           hover:scale-105 focus:outline-none focus:ring-2 focus:ring-green-700 focus:ring-opacity-50"
           onClick={() =>
