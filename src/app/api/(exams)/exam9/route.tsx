676ba41506a02b17ca5c518ad5db4b739f94a6ad
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 17:33:25 +0330

    fill exam 9

--- src/app/api/(exams)/exam9/route.tsx
+++ src/app/api/(exams)/exam9/route.tsx
@@ -4,67 +4,67 @@ export function GET(request: Request) {
       {
         id: 1,
         question: "Ernesto would constantly ______ his father's questions.",
-        answer: "weird",
+        answer: "ignore",
       },
       {
         id: 2,
         question:
           "Historical ______ are kept in a special section of the library.",
-        answer: "valid",
+        answer: "documents",
       },
       {
         id: 3,
         question: "Great scientific progress has been made in this ______",
-        answer: "obvious",
+        answer: "century",
       },
       {
         id: 4,
         question: "The massive wrestler accepted the ______ of the newcomer.",
-        answer: "mediocre",
+        answer: "challenge",
       },
       {
         id: 5,
         question: "Not wearing warm clothing was the ______ of his illness.",
-        answer: "reluctant",
+        answer: "source",
       },
       {
         id: 6,
         question:
           "When do you expect to ______ your investigation of the case?",
-        answer: "exaggerate",
+        answer: "conclude",
       },
       {
         id: 7,
         question: "It is ______ that this restaurant's food is delicious.",
-        answer: "security",
+        answer: "undeniable",
       },
       {
         id: 8,
         question: "Lena showed ______ a of good judgment.",
-        answer: "bulky",
+        answer: "lack",
       },
       {
         id: 9,
         question:
           "Everyone who lived in the ______ of the bomb test was in peril.",
-        answer: "amateur",
+        answer: "vicinity",
       },
       {
         id: 10,
         question:
           "Anita's habit of interrupting him sent her husband into a ______",
-        answer: "variety",
+        answer: "rage",
       },
       {
         id: 11,
         question: "My nephew was given a set of ______ soldiers for Christmas.",
-        answer: "prominent",
+        answer: "miniature",
       },
       {
         id: 12,
         question:
           "When you are tired it is hard to ______ staying in bed all day.",
-        answer: "survive",
+        answer: "resist",
       },
     ],
   };
