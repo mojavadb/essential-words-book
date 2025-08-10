c9158492907b9ed75ee725369cff5025b9c10940
mojavadb <m.j.babaee98@gmail.com>
2025-08-11 01:47:01 +0330

    fill exam 38

--- src/app/api/(exams)/exam38/route.tsx
+++ src/app/api/(exams)/exam38/route.tsx
@@ -3,63 +3,72 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question:
+          "The principal probed the cause of the ______ in the cafeteria.",
+        answer: "tumult",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question: "A ______ of insects descended on the picnic food.",
+        answer: "swarm",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question:
+          "When asked for their ______ on the agreement, the members gave their approval spontaneously.",
+        answer: "verdict",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question:
+          "The first project in our creative writing class was a ______ of a close friend or relative.",
+        answer: "biography",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question:
+          "Until the flood waters ______ , the authorities prohibited anyone from returning to the vicinity.",
+        answer: "receded",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question:
+          "Mr. Finley was redeemed in the eyes of his employees by his ______ in overlooking their costly error.",
+        answer: "charity",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question: "The grateful traveler would ______ in prayer every night. ",
+        answer: "kneel",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question: "Mother is an expert at soothing our ______ spirits.",
+        answer: "dejected",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question:
+          "It is absurd to surrender your rights to ______ a when you have abundant reason to remain free.",
+        answer: "tyrant",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question:
+          "We faced the dilemma of being ______ in the downpour while we covered our boat or having to bail the water out of the boat after the rain had ceased.",
+        answer: "drenched",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question:
+          "Melinda shrieked as the unstable pedestrian ______ into the path of the oncoming car.",
+        answer: "wobbled",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "A glance from the mother was enough of a reminder to bring the ______ child back to her side.",
+        answer: "obedient",
       },
     ],
   };
