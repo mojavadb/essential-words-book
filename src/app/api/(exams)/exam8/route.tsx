bcdf4ed839d2e585216304a43369afef12a01247
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 17:30:49 +0330

    fill exam 8

--- src/app/api/(exams)/exam8/route.tsx
+++ src/app/api/(exams)/exam8/route.tsx
@@ -4,64 +4,67 @@ export function GET(request: Request) {
       {
         id: 1,
         question:
-          "Most people agreed that he was a looking man because of the long red beard.",
+          "Most people agreed that he was a ______ looking man because of the long red beard.",
         answer: "weird",
       },
       {
         id: 2,
         question:
-          "Chuck's reason for quitting his job was ; he was not being paid.",
+          "Chuck's reason for quitting his job was ______ ; he was not being paid.",
         answer: "valid",
       },
       {
         id: 3,
-        question: "The answer to the question was so that everyone knew it.",
+        question:
+          "The answer to the question was so ______ that everyone knew it.",
         answer: "obvious",
       },
       {
         id: 4,
-        question: "The tennis player would never make the Olympic squad.",
+        question:
+          "The ______ tennis player would never make the Olympic squad.",
         answer: "mediocre",
       },
       {
         id: 5,
-        question: "She was to take on any more responsibilities at work.",
+        question:
+          "She was ______ to take on any more responsibilities at work.",
         answer: "reluctant",
       },
       {
         id: 6,
-        question: "People often tend to stories they hear.",
+        question: "People often tend to ______ stories they hear.",
         answer: "exaggerate",
       },
       {
         id: 7,
-        question: "The bank is kept under very tight ___ _",
+        question: "The bank is kept under very tight ______",
         answer: "security",
       },
       {
         id: 8,
-        question: "Because the box was so it took two men to lift it.",
+        question: "Because the box was so ______ it took two men to lift it.",
         answer: "bulky",
       },
       {
         id: 9,
         question:
-          "Even though he was not a professional, the photographer entered the contest.",
+          "Even though he was not a professional, the ______ photographer entered the contest.",
         answer: "amateur",
       },
       {
         id: 10,
-        question: "A wide of shows is playing at the concert hall.",
+        question: "A wide ______ of shows is playing at the concert hall.",
         answer: "variety",
       },
       {
         id: 11,
-        question: "Mrs. Meyers is a member of the staff.",
+        question: "Mrs. Meyers is a ______ member of the staff.",
         answer: "prominent",
       },
       {
         id: 12,
-        question: "We all hoped that the small boat would the storm. ",
+        question: "We all hoped that the small boat would ______ the storm. ",
         answer: "survive",
       },
     ],
