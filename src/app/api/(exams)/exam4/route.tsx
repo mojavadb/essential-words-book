5a5647c92e2f9120c5d9bbfb9680ee0525fcf2bf
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 16:36:23 +0330

    fill exam 4

--- src/app/api/(exams)/exam4/route.tsx
+++ src/app/api/(exams)/exam4/route.tsx
@@ -3,63 +3,73 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question:
+          "If we have one more hot, ______ day, you will be able to persuade* me to move to Alaska.",
+        answer: "humid",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question:
+          "In the show the magician waved his wand to make a lady ______",
+        answer: "vanish",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question:
+          "The hair on his head was so ______ , a special pair of scissors was used to thin it.",
+        answer: "dense",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question:
+          "Since he has passed all his subjects, I'll ______ that he will graduate.",
+        answer: "predict",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question:
+          "The ______ in the movie was played by an actor who was able to look mean.",
+        answer: " villain",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question: "______ rose out of the valve on top of the steam engine.",
+        answer: " Vapor",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question:
+          "The basketball player was ______ ; he could practically drop the ball through the hoop.",
+        answer: "enormous",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question:
+          "What ______ can you suggest to explain the frequent changes in women's clothing?",
+        answer: " theory",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question: "Why don't you ______ all the space on that page?",
+        answer: "utilize",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question:
+          "Sooner or later the elevator will ______ and we'll be able to go up.",
+        answer: "descend",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question:
+          "I heard a doctor on a television show say that if we ______ one slice of bread each day, we'll lose weight.",
+        answer: "eliminate",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "Copies of some magazines are so scarce,* the librarian won't allow them to ______ ",
+        answer: "circulate",
       },
     ],
   };
