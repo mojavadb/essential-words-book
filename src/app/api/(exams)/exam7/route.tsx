37177d19439e20fe09c892d2a5cc704e02f8944b
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 17:10:50 +0330

    fill exam 7

--- src/app/api/(exams)/exam7/route.tsx
+++ src/app/api/(exams)/exam7/route.tsx
@@ -3,63 +3,69 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question:
+          "We have tried for over 200 years to ______ the United States Constitution.",
+        answer: "preserve",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question: "The ______ weather predictions upset him.",
+        answer: "gloomy",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question:
+          "Will Karen ______ to having her baby picture published in the school newspaper?",
+        answer: "consent",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question: "I found a ______ collection of old books in the attic.",
+        answer: "unique",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question: "Dave knew that if he mistreated her, she would ______ it.",
+        answer: "resent",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question:
+          "The president ______ the criminal activities that were going on.",
+        answer: "denounced",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question:
+          "Lori feared that if she walked the streets, she would be ______ ",
+        answer: "molested",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question: "Owning a house created ______ difficulties.",
+        answer: "unforeseen",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question: "The new movie invited a ______ of disapproval.",
+        answer: "torrent",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question:
+          "A telephone call told us that the employees' picnic was ______ until next week.",
+        answer: "postponed",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question: "The ______ was filled with records of the past.",
+        answer: "capsule",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "It was a ______ job for just one person to unload the big truck.",
+        answer: "massive",
       },
     ],
   };
