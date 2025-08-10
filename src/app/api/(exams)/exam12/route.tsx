c87403df085814c5920246da702ff4272d505f2f
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 17:52:56 +0330

    fill exam 12

--- src/app/api/(exams)/exam12/route.tsx
+++ src/app/api/(exams)/exam12/route.tsx
@@ -3,63 +3,66 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question: "The doctor was completely with the dying man.",
+        answer: "frank",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question: "In an whisper, Maria called for my attention.",
+        answer: "audible",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question:
+          "We didn't need any evidence* to see that the poor man was in need of money and food.",
+        answer: "urgent",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question: "All his life the child was used to living in areas.",
+        answer: "urban",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question: "Dry matches to the campfire were sought by the boy scout.",
+        answer: "ignite",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question: "Smoking is in the medical building.",
+        answer: "prohibited",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question: "Gasoline fumes help to the air.",
+        answer: "pollute",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question:
+          "The in the number of people voting in national elections is due to lack of interest.",
+        answer: "decrease",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question: "Some citizens believe that we will never be able to war.",
+        answer: "abolish",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question: "The of New York City is about eight million people.",
+        answer: "population",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question: "In the comics, Superman never his true identity.",
+        answer: "reveals",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "They needed an supply of water to last for the entire trip through the desert.",
+        answer: "adequate",
       },
     ],
   };
