08766180f1ee8ced661f0fdd851a480e3d694762
mojavadb <m.j.babaee98@gmail.com>
2025-08-10 21:00:47 +0330

    fill exam 21

--- src/app/api/(exams)/exam21/route.tsx
+++ src/app/api/(exams)/exam21/route.tsx
@@ -3,63 +3,66 @@ export function GET(request: Request) {
     exam: [
       {
         id: 1,
-        question: "",
-        answer: "",
+        question:
+          "The first time a message was ______ over a telegraph was in 1840.",
+        answer: "transmitted",
       },
       {
         id: 2,
-        question: "",
-        answer: "",
+        question: "Can you ______ that this is your handwriting?",
+        answer: "confirm",
       },
       {
         id: 3,
-        question: "",
-        answer: "",
+        question: "I took a ______ to avoid traffic.",
+        answer: "detour",
       },
       {
         id: 4,
-        question: "",
-        answer: "",
+        question: "We were all very ______ after the long trip.",
+        answer: "weary",
       },
       {
         id: 5,
-        question: "",
-        answer: "",
+        question: "There is ______ in being a good listener.",
+        answer: "merit",
       },
       {
         id: 6,
-        question: "",
-        answer: "",
+        question: "The judge listened to the burglar's ______ of not guilty.",
+        answer: "plea",
       },
       {
         id: 7,
-        question: "",
-        answer: "",
+        question: "The runner and catcher were about to ______ at home plate.",
+        answer: "collide",
       },
       {
         id: 8,
-        question: "",
-        answer: "",
+        question: "Tyrone was ______ that he didn't have any more work to do.",
+        answer: "relieved",
       },
       {
         id: 9,
-        question: "",
-        answer: "",
+        question:
+          "When you run a company you have at least one new ______ every day to solve.",
+        answer: "dilemma",
       },
       {
         id: 10,
-        question: "",
-        answer: "",
+        question: "Did you ______ our reservations at the hotel?",
+        answer: "verify",
       },
       {
         id: 11,
-        question: "",
-        answer: "",
+        question: "We were ______ as to who murdered the wretched old man.",
+        answer: "baffled",
       },
       {
         id: 12,
-        question: "",
-        answer: "",
+        question:
+          "After having been married to Arthur for thirty years, Selma could ______ everything he was going to say.",
+        answer: "anticipate ",
       },
     ],
   };
