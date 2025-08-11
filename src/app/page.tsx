970e603b3dbe04b65c0cbcc256a1d9a8f1a08f82
mojavadb <m.j.babaee98@gmail.com>
2025-08-11 13:29:34 +0330

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,11 @@ import Status from "@/components/Status";
 
 export default async function Home() {
   return (
-    <main>
+    <main className="relative">
+      <div
+        className="absolute bg-center w-full h-full -top-160 -z-1 bg-repeat bg-cover"
+        style={{ backgroundImage: 'url("/header-bg-color.png")' }}
+      />
       <Header />
       <Introduction />
       <Status />
